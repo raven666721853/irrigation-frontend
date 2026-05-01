331688387c1545a15014ef665e7afd5627b2cc79
raven666721853 <takihamdi027@gmail.com>
2026-05-01 16:18:15 +0100

    final fix api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-irrigation-backend-wra6.onrender.com"
+  baseURL: "https://smart-irrigation-backend-wra6.onrender.com/api",
 });
 
 export default API;
